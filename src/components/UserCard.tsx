191fda207229daf6776daf1c0a2ac6bc7a48eb85
Marcelina Sitko <61546640+marcelinax@users.noreply.github.com>
2021-05-10 15:51:25 +0200

    :8ball: fix company name in user card

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -10,9 +10,6 @@ import { useSelector } from "react-redux";
 
 const UserCard: FC = () => {
   const loggedUser = useSelector<RootState, User>((store) => store.loggedUser);
-  const companies = useSelector<RootState, Company[]>(
-    (store) => store.companies
-  );
 
   return (
     <div className="user-card">
@@ -20,7 +17,7 @@ const UserCard: FC = () => {
         <UserAvatar width={80} height={80} />
         <p className="user-name">{loggedUser.name}</p>
         <p className="desc">
-          {companies.length > 0 && companies.filter((c) => c.id === 1)[0].name}
+          {loggedUser.company.name}
         </p>
       </div>
       <hr />
